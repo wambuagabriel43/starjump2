ac5f11ff15f8056a26e2149b11229a2aaa1bd2bc
wambuagabriel43 <42387606+wambuagabriel43@users.noreply.github.com>
2025-07-16 19:04:58 +0300

    Fix home page content error by removing unnecessary usePageContent

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { usePageContent } from '../hooks/usePageContent';
 import ImageSlider from '../components/ImageSlider';
 import UpcomingEvents from '../components/UpcomingEvents';
 import InfoSection from '../components/InfoSection';
@@ -7,23 +6,13 @@ import BookingCTA from '../components/BookingCTA';
 import ShopPreview from '../components/ShopPreview';
 
 const Home: React.FC = () => {
-  const { content: pageContent, loading } = usePageContent('home');
-
-  if (loading) {
-    return (
-      <div className="min-h-screen flex items-center justify-center">
-        <div className="w-8 h-8 border-4 border-white border-t-transparent rounded-full animate-spin"></div>
-      </div>
-    );
-  }
-
   return (
     <main>
       <ImageSlider />
       <UpcomingEvents />
-      <InfoSection pageContent={pageContent} />
-      <BookingCTA pageContent={pageContent} />
-      <ShopPreview pageContent={pageContent} />
+      <InfoSection />
+      <BookingCTA />
+      <ShopPreview />
     </main>
   );
 };
